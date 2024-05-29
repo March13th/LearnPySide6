bdad8331f837d40332ae6d889b1ac5dd0b0c4f03
March13th <march_13th@aliyun.com>
2024-05-29 23:55:10 +0800

    update

--- basic/signals_and_slots_1b.py
+++ basic/signals_and_slots_1b.py
@@ -11,10 +11,20 @@ class MainWindow(QMainWindow):
 
         button = QPushButton("Press Me!")
         button.setCheckable(True)
-        button.clicked.connect()
-        button.clicked.connect()
+        button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
 
         self.setCentralWidget(button)
 
     def the_button_was_clicked(self):
         print('Clicked!')
+
+    def the_button_was_toggled(self,checked):
+        print('Checked?',checked)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
